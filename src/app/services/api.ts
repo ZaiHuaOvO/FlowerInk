067def4d75e8c26819a8626bd340a7a3505884dc
ZaiHua <446840401@qq.com>
2024-10-10 16:54:31 +0800

    更新后端接口

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,8 +1,8 @@
 // src/app/config/api.ts
 
 export const API = {
-  BASE_URL: 'http://localhost:8089',
-  // BASE_URL: 'http://47.109.133.108/api',
+  // BASE_URL: 'http://localhost:8089',
+  BASE_URL: 'http://47.109.133.108/api',
   BLOG: '/blog',
   TAG: '/blog/tag',
   INFO: '/blog/info',
